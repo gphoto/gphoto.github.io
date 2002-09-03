a0f32858196e95373ceb08b7cd82a52803101f86
Hubert Figuière <hub@figuiere.net>
2002-09-03 08:14:44 +0000

    mailing list page
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/website@5189 67ed7778-7388-44ab-90cf-0a291f65f57c

---
+++ mailinglists/index.php
@@ -0,0 +1,51 @@
+<?
+	require ("../include.php");
+
+	printHeader ("Mailinglists", "", 1);
+
+	printMenu ("mailinglists");
+?>
+<table cellpadding="3" cellspacing="3">
+<tr><td>
+<h2>gPhoto mailing lists</h2>
+</td></tr>
+<tr>
+	<td class="text">
+        <h3>gphoto@gphoto.org</h3>
+        <p>General list where gphoto user oriented issues are discussed, and
+        announcements to the general public are frequently posted by the 
+        gphoto contributors. </p>
+
+	<p><a href="http://marc.theaimsgroup.com/?l=gphoto">Archives</a></p>
+        <p>Subscribing:
+	<br />Send email to 
+	<a href="mailto:gphoto-subscribe@gphoto.org">gphoto-subscribe@gphoto.org</a></p>
+
+        <p>Unsubscribing:
+	<br />
+	Send email to 
+	<a href="mailto:gphoto-unsubscribe@gphoto.org">gphoto-unsubscribe@gphoto.org</a>
+	</p>
+
+        <h3>gphoto-devel@gphoto.org</h3>
+        <p>
+        Developer mailinglist, with bug reports, camera reports, etc.
+        If you would like to get involved in gphoto
+        developments, then you may want to read it.</p>
+                        
+	<p><a href="http://marc.theaimsgroup.com/?l=gphoto-devel">Archives</a></p>
+
+        <p>Subscribing:
+	<br />Send email to 
+	<a href="mailto:gphoto-devel-subscribe@gphoto.org">gphoto-devel-subscribe@gphoto.org</a></p>
+
+        <p>Unsubscribing:
+	<br />Send email to 
+	<a href="mailto:gphoto-devel-unsubscribe@gphoto.org">gphoto-devel-unsubscribe@gphoto.org</a></p>
+
+	</td>
+</tr>
+</table>
+<?
+	printFooter ();
+?>
