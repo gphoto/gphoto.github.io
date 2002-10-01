25c44f4af306deffd3f59f0f0e31e9017aed0d6f
Hubert Figuière <hub@figuiere.net>
2002-10-01 12:01:15 +0000

    New libgphoto2 blurb
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/website@5341 67ed7778-7388-44ab-90cf-0a291f65f57c

--- proj/libgphoto2/index.php
+++ proj/libgphoto2/index.php
@@ -6,13 +6,34 @@
 	printMenu ("proj");
 ?>
 
+<table cellpadding="3" cellspacing="3" width="100%">
+<tr class="text"><td>
 
+<p>libgphoto2 is the core library designed to allow access to digital camera
+by external programs. Here is an overview of the global architecture:</p>
+<p align="center">
+<img src="architecture.png" alt="libgphoto2 architecture" />
+</p>
+<p>It abstracts communication ports and camera protocol, to allow a complete
+modularity. To support a new communication physical layer (like IEEE1394), 
+just add a new port to libgphoto2_port. To support a new kind a digital camera,
+just provide a new camlib with the required callbacks. All of this will
+be transparent to client (programs that call libgphoto2</p>
+<p>For a deeper view of the inner working of libgphoto2, please read the 
+apropriate 
+<a href="/doc/manual/developer-docs.html">chapter</a>
+in the <a href="/doc/manual/">gPhoto2 manual</a>
+</p>
+</td>
+</tr>
+</table>
 
 <h2>Supported Cameras</h2>
 <table cellpadding="3" cellspacing="3" width="100%">
 <tr class="text"><td>
 
-<p>Here is a <a href="support.php">list</a> of supported cameras as of current release.
+<p>Here is a <a href="support.php">list</a> of supported cameras as of current 
+release.
 </p>
 </td>
 </tr>
