f60f61ece6328b2dfd33e562444a0bc849973b33
Hubert Figuière <hub@figuiere.net>
2002-10-18 07:10:59 +0000

    Now redirect to the FAQ chapter in the manual
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/website@5403 67ed7778-7388-44ab-90cf-0a291f65f57c

--- doc/faq/index.php
+++ doc/faq/index.php
@@ -1,16 +1,3 @@
 <?
-	require ("../../include.php");
-
-	printHeader ("Documentation :: faq", "", 1);
-
-	printMenu ("doc");
-?>
-
-<p>The FAQ is contained in the 
-<a href="../manual/FAQ.html">FAQ chapter</a> of 
-<a href="../manual/">the manual</a>.
-</p>
-
-<?
-	printFooter ();
+	Header("Location: ../manual/FAQ.html");
 ?>
