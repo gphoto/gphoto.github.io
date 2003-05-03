5d1c77eb5d38e93b89deb29e597eab51a8098b0a
Christophe Barbé <christophe.barbe@ufies.org>
2003-05-03 15:48:28 +0000

    Updated to reflect the new mailing-lists on sf.net
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/website@6363 67ed7778-7388-44ab-90cf-0a291f65f57c

--- mailinglists/index.php
+++ mailinglists/index.php
@@ -11,37 +11,33 @@
 </td></tr>
 <tr>
 	<td class="text">
-        <h3>gphoto@gphoto.org</h3>
+    
+		  <h3>gphoto-user@lists.sf.net</h3>
         <p>General list where gphoto user oriented issues are discussed, and
         announcements to the general public are frequently posted by the 
         gphoto contributors. </p>
-
-	<p><a href="http://marc.theaimsgroup.com/?l=gphoto">Archives</a></p>
+                        
         <p>Subscribing:
 	<br />Send email to 
-	<a href="mailto:gphoto-subscribe@gphoto.org">gphoto-subscribe@gphoto.org</a></p>
+	<a href="mailto:gphoto-user-subscribe@lists.sf.net">gphoto-user-subscribe@lists.sf.net</a></p>
 
         <p>Unsubscribing:
-	<br />
-	Send email to 
-	<a href="mailto:gphoto-unsubscribe@gphoto.org">gphoto-unsubscribe@gphoto.org</a>
-	</p>
+	<br />Send email to 
+	<a href="mailto:gphoto-user-unsubscribe@lists.sf.net">gphoto-user-unsubscribe@lists.sf.net</a></p>
 
-        <h3>gphoto-devel@gphoto.org</h3>
-        <p>
-        Developer mailinglist, with bug reports, camera reports, etc.
+        
+		  <h3>gphoto-devel@lists.sf.net</h3>
+        <p>Developer mailinglist, with bug reports, camera reports, etc.
         If you would like to get involved in gphoto
         developments, then you may want to read it.</p>
                         
-	<p><a href="http://marc.theaimsgroup.com/?l=gphoto-devel">Archives</a></p>
-
         <p>Subscribing:
 	<br />Send email to 
-	<a href="mailto:gphoto-devel-subscribe@gphoto.org">gphoto-devel-subscribe@gphoto.org</a></p>
+	<a href="mailto:gphoto-devel-subscribe@lists.sf.net">gphoto-devel-subscribe@lists.sf.net</a></p>
 
         <p>Unsubscribing:
 	<br />Send email to 
-	<a href="mailto:gphoto-devel-unsubscribe@gphoto.org">gphoto-devel-unsubscribe@gphoto.org</a></p>
+	<a href="mailto:gphoto-devel-unsubscribe@lists.sf.net">gphoto-devel-unsubscribe@lists.sf.net</a></p>
 
         <h3>gphoto-cvs@lists.sf.net</h3>
         <p>
@@ -56,6 +52,39 @@
         <p>Unsubscribing:
 	<br />Send email to 
 	<a href="mailto:gphoto-cvs-unsubscribe@lists.sf.net">gphoto-cvs-unsubscribe@lists.sf.net</a></p>
+
+        <h3>gphoto@gphoto.org is DEPRECATED</h3>
+        <p>General list where gphoto user oriented issues are discussed, and
+        announcements to the general public are frequently posted by the 
+        gphoto contributors. </p>
+
+	<p><a href="http://marc.theaimsgroup.com/?l=gphoto">Archives</a></p>
+        <!--<p>Subscribing:
+	<br />Send email to 
+	<a href="mailto:gphoto-subscribe@gphoto.org">gphoto-subscribe@gphoto.org</a></p>
+
+        <p>Unsubscribing:
+	<br />
+	Send email to 
+	<a href="mailto:gphoto-unsubscribe@gphoto.org">gphoto-unsubscribe@gphoto.org</a>
+	</p>-->
+
+        <h3>gphoto-devel@gphoto.org is DEPRECATED</h3>
+        <p>
+        Developer mailinglist, with bug reports, camera reports, etc.
+        If you would like to get involved in gphoto
+        developments, then you may want to read it.</p>
+                        
+	<p><a href="http://marc.theaimsgroup.com/?l=gphoto-devel">Archives</a></p>
+
+        <!--<p>Subscribing:
+	<br />Send email to 
+	<a href="mailto:gphoto-devel-subscribe@gphoto.org">gphoto-devel-subscribe@gphoto.org</a></p>
+
+        <p>Unsubscribing:
+	<br />Send email to 
+	<a href="mailto:gphoto-devel-unsubscribe@gphoto.org">gphoto-devel-unsubscribe@gphoto.org</a></p>
+	-->
 
 	</td>
 </tr>
