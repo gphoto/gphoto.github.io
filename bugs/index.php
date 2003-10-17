d5346641bad935ff710b2a9c1182235d09ec2937
Hubert Figuière <hub@figuiere.net>
2003-10-17 15:21:53 +0000

    bugs redirector http://www.gphoto.org/bugs/?id=452042 to load bug 452042 http://www.gphoto.org/bugs/ brings to the tracker
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/website@6833 67ed7778-7388-44ab-90cf-0a291f65f57c

--- bugs/index.php
+++ bugs/index.php
@@ -1,4 +1,8 @@
 <?
-	
-	Header("Location: http://sourceforge.net/tracker/?group_id=8874&atid=108874");
+	if ($_GET['id'] != '') {
+		Header("Location: http://sourceforge.net/tracker/?group_id=8874&atid=108874&func=detail&aid=". $_GET['id']);
+	}
+	else {
+		Header("Location: http://sourceforge.net/tracker/?group_id=8874&atid=108874");
+	}
 ?>
