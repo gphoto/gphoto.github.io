b727b58708fe14f7eb30d6286b1d1e99052258ca
Hubert Figuière <hub@figuiere.net>
2003-10-20 07:46:15 +0000

    direct access to RFE, like bugs
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/website@6872 67ed7778-7388-44ab-90cf-0a291f65f57c

---
+++ rfe/index.php
@@ -0,0 +1,8 @@
+<?
+	if ($_GET['id'] != '') {
+		Header("Location: http://sourceforge.net/tracker/?group_id=8874&atid=358874&func=detail&aid=". $_GET['id']);
+	}
+	else {
+		Header("Location: http://sourceforge.net/tracker/?group_id=8874&atid=358874");
+	}
+?>
