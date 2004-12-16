a46b35c515f28d93a0e964bb3a3120636045e9c2
Hubert Figuière <hub@figuiere.net>
2004-12-16 03:23:23 +0000

    Update latest release #
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/website@7425 67ed7778-7388-44ab-90cf-0a291f65f57c

--- download/index.php
+++ download/index.php
@@ -1,6 +1,7 @@
 <?
-	$current_release="2.1.4";
+	$current_release="2.1.5";
 
+	$all_releases["2.1.5"]=290239;
 	$all_releases["2.1.4"]=209817;
 	$all_releases["2.1.3"]=191969;
 	$all_releases["2.1.2"]=174065;
