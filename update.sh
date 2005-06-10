37ef67cc2100e184ee46e9eb581df1a90dce80da
Hans Ulrich Niedermann <hun@n-dimensional.de>
2005-06-10 10:12:24 +0000

    update update.sh: fix permissions correctly, and install correct set of files
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/website@8056 67ed7778-7388-44ab-90cf-0a291f65f57c

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 
 # the file .website_login contain the sf.net login name.
 # create it in your local tree.
@@ -10,11 +10,12 @@ else
 	SERVER_HOST=gphoto.sourceforge.net
 fi
 VERBOSE=""
-#VERBOSE=-v
+VERBOSE="-v"
+#VERBOSE="-v --dry-run --delete"
 
 echo "Synchronizing..."
-rsync -az $VERBOSE --exclude=CVS "--exclude=*.sh" "--exclude=*.swp" --exclude="*~" --exclude=.website_login --exclude=.cvsignore -e ssh . ${SERVER_HOST}:/home/groups/g/gp/gphoto/htdocs
+rsync -az $VERBOSE --progress --exclude='CVS' --exclude='patch' --exclude='.#*' --exclude='*.patch' --exclude='*.sh' --exclude='*.swp' --exclude='*~' --exclude='.website_login' --exclude='.cvsignore' -e ssh . "${SERVER_HOST}:/home/groups/g/gp/gphoto/htdocs"
 
 echo "Changing perms..."
-ssh ${SERVER_HOST} "cd /home/groups/g/gp/gphoto/htdocs ; chgrp -R gphoto *; chmod -R g+w *"
+ssh "${SERVER_HOST}" "find /home/groups/g/gp/gphoto/htdocs \( -type d -exec chgrp gphoto {} \; -exec chmod g+rwxs,a+rx {} \; \) -or \( -type f -exec chgrp gphoto {} \; -exec chmod g+rw {} \; \( -name '*.html' -or -name '*.png' -or -name '*.php' -or -name '*.jpg' \) -exec chmod a+r {} \; \)"
 
