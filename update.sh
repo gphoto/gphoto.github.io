cef97819b2efd0b6555a925d12b63ecfaeee78bf
Hans Ulrich Niedermann <hun@n-dimensional.de>
2005-06-16 03:03:01 +0000

    permission fixup for CSS files
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/website@8105 67ed7778-7388-44ab-90cf-0a291f65f57c

--- update.sh
+++ update.sh
@@ -17,5 +17,5 @@ echo "Synchronizing..."
 rsync -az $VERBOSE --progress --exclude='CVS' --exclude='patch' --exclude='.#*' --exclude='*.patch' --exclude='*.sh' --exclude='*.swp' --exclude='*~' --exclude='.website_login' --exclude='.cvsignore' -e ssh . "${SERVER_HOST}:/home/groups/g/gp/gphoto/htdocs"
 
 echo "Changing perms..."
-ssh "${SERVER_HOST}" "find /home/groups/g/gp/gphoto/htdocs \( -type d -exec chgrp gphoto {} \; -exec chmod g+rwxs,a+rx {} \; \) -or \( -type f -exec chgrp gphoto {} \; -exec chmod g+rw {} \; \( -name '*.html' -or -name '*.png' -or -name '*.php' -or -name '*.jpg' \) -exec chmod a+r {} \; \)"
+ssh "${SERVER_HOST}" "find /home/groups/g/gp/gphoto/htdocs \( -type d -exec chgrp gphoto {} \; -exec chmod g+rwxs,a+rx {} \; \) -or \( -type f -exec chgrp gphoto {} \; -exec chmod g+rw {} \; \( -name '*.html' -or -name '*.png' -or -name '*.php' -or -name '*.jpg' -or -name '*.css' \) -exec chmod a+r {} \; \)"
 
