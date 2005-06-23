03025c3628e6f81b6c608fa77bef80450147b76e
Hans Ulrich Niedermann <hun@n-dimensional.de>
2005-06-23 12:43:26 +0000

    added a few comments for improvements, and improved a few existing sentences
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/website@8129 67ed7778-7388-44ab-90cf-0a291f65f57c

--- manufacturers/index.php
+++ manufacturers/index.php
@@ -30,13 +30,15 @@ the documentation has been pulled off the website.
 </p>
 
 <h4>Konica</h4>
-<p>Konica has provided reference and partial implementation for the konica driver.</p>
+<p>Konica has provided reference and partial implementation for the konica driver<!--
+FIXME: In what year? For what cameras? -->.</p>
 
 <p>Howerver, Konica has not responded to the request for the QM-150 camera driver in 2003. 
 Developers were just ignored.</p>
 
 <p>Most recent Konica cameras did use USB Mass Storage. Konica is now merged with Konica-Minolta
-and it is a different organization.</p>
+and it is a different organization<!--
+FIXME: Different from when? 2003? Or when they did provide information? -->.</p>
 
 <h3>Manufacturers that did NOT provide help</h3>
 
@@ -46,10 +48,18 @@ real evidence of their lack of cooperation.</p>
 <h4>Canon</h4>
 <p>Canon has not provided any help to the gphoto project. Their proprietary SDK does
 not have support for Linux, and does not have the required documentation to implement
-the functionnality.</p>
+the functionality.</p>
 
-<p>All their recent cameras support PTP standard protocol and work almost out of the
-box with gphoto. But for advanced control it is another story.</p>
+<!-- paragraph uncommented until I find the mail from Canon which said that
+<p>When asked specifically about software for Unix or Linux, Canon mentioned that
+they only supported Windows, and that gphoto might do the job. They continued that
+they didn't support the 3rd party product gphoto and couldn't make any claims as to
+whether, how, and how good gphoto may work.</p>
+-->
+
+<p>All their recent cameras support PTP standard protocol and thus the basic functionality
+of downloading files works almost out of the box with gphoto. But advanced control is a
+different story.</p>
 
 <h4>Epson</h4>
 <p>Epson advertise us in the 
@@ -64,22 +74,23 @@ and scanners, with a <a href="http://www.epsondevelopers.com/noGuide.jsp">long l
 <h4>Olympus</h4>
 <p>When requesting the documentation because their SDK only support Windows, 
 here is the answer I got on June 22, 2005:</p>
-<pre>
+<pre class="programlisting">
 We have not released the communication specification publically. You can
 try the gPhoto project, www.gPhoto.org, who has claimed to reverse engineer
 the interface.
 </pre>
 <p>A reply has been sent, asking about documentation to fix the software (we have issues and al.).
 Got this new reply:</p>
-<pre>
+<pre class="programlisting">
 I'm afraid we do not publicize the interface. That is why gPhoto had to
 reverse engineer it.
 </pre>
 
-<p>Charming ?</p>
+<p>Charming?</p>
 
-<p>Olympus camera support for quite a long time USB Mass Storage, and more recently PTP, making
-gphoto unecessary or work out of the box (for PTP). But for remote control, 
+<p>Olympus camera supports have been supporting USB Mass Storage for quite some time, and PTP
+more recently. This makes gphoto unecessary (Mass Storage) or work out of the box (for PTP).
+But for remote control, 
 <a href="http://developer.olympus.com/cameras_25.html">supported cameras</a> needs documentation
 and that is what they are currently declining.</p>
 
@@ -88,6 +99,8 @@ and that is what they are currently declining.</p>
 <p>Documentation has been requested, but require signing a NDA. Nikon has been requested clarifications
 about the NDA (sent June 17 2005) to know if Open Source software can be written from it, no answer so 
 far (June 22 2005)...</p>
+
 <?
 	printFooter ();
 ?>
+
