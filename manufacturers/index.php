d566be0911f35c12ade9fc880ea483d93281f0ed
Hans Ulrich Niedermann <hun@n-dimensional.de>
2005-06-23 13:22:32 +0000

    improved sentences
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/website@8132 67ed7778-7388-44ab-90cf-0a291f65f57c

--- manufacturers/index.php
+++ manufacturers/index.php
@@ -6,30 +6,47 @@
 	printMenu ("manufacturers");
 ?>
 
-<h2>About manufacturers</h2>
+<p>
+On this page, we try to list the cooperation we have received and are receiving 
+from camera manufacturers, or rather the almost total lack thereof.
+So this page is mostly intended to give them bad press.
+Unfortunately, many companies do not listen to anything else.
+</p>
 
 <p>
-We'll try to list here the cooperation, or mostly lack thereoff of cooperation from
-camera manufacturers. This page is mostly intended to give them bad press. This fact is
-not new, as they don't document anything, to supposedly protect their "intellectual property". 
-For example RAW files are not documented, that is why
-<a href="http://www.openraw.org/">OpenRAW</a> has been created.
-</p>
-<p>We just hope to have you help make a good choice to support open source solutions.</p>
+The facts are not new, as the manufacturers don't document anything, supposedly in
+order to protect their "intellectual property". Examples:</p>
+<ul>
+<li>RAW files ("digital negatives") are not documented, which is why
+    <a href="http://www.openraw.org/">OpenRAW</a> has been created.
+</li>
+<li>Communication protocols are not documented, which is why we started this page
+</li>
+</ul>
 
-<h3>Manufacturers that did provide help</h3>
+<p>We hope this page helps you make a good buying decision to support
+open source solutions.</p>
+
+
+<!-- ============================================================ --> 
+
+<h2>Manufacturers that have provided help</h2>
 
 <p>
 The list is quite small:
 </p>
 
-<h4>Kodak</h4>
+<!-- ========================= -->
+
+<h3>Kodak</h3>
 <p>Kodak has published documentation for the protocols they
 used in consumer cameras, prior their switch to PTP. Since, due to various company reorganization
 the documentation has been pulled off the website.
 </p>
 
-<h4>Konica</h4>
+<!-- ========================= -->
+
+<h3>Konica</h3>
 <p>Konica has provided reference and partial implementation for the konica driver<!--
 FIXME: In what year? For what cameras? -->.</p>
 
@@ -40,28 +57,36 @@ Developers were just ignored.</p>
 and it is a different organization<!--
 FIXME: Different from when? 2003? Or when they did provide information? -->.</p>
 
-<h3>Manufacturers that did NOT provide help</h3>
+
+<!-- ============================================================ --> 
+
+<h2>Manufacturers that have NOT provided help</h2>
 
 <p>This is where the list goes. In fact, it is almost every manufacturers. We'll just provide
 real evidence of their lack of cooperation.</p>
 
-<h4>Canon</h4>
+<!-- ========================= -->
+
+<h3>Canon</h3>
 <p>Canon has not provided any help to the gphoto project. Their proprietary SDK does
 not have support for Linux, and does not have the required documentation to implement
 the functionality.</p>
 
 <!-- paragraph uncommented until I find the mail from Canon which said that
 <p>When asked specifically about software for Unix or Linux, Canon mentioned that
-they only supported Windows, and that gphoto might do the job. They continued that
-they didn't support the 3rd party product gphoto and couldn't make any claims as to
-whether, how, and how good gphoto may work.</p>
+they only supported Windows, but that there as a 3rd party solution called gphoto
+which might do the job. They continued that they didn't support the 3rd party products
+like gphoto and thus couldn't make any claims as to whether, how, and how good gphoto
+may work.</p>
 -->
 
 <p>All their recent cameras support PTP standard protocol and thus the basic functionality
 of downloading files works almost out of the box with gphoto. But advanced control is a
 different story.</p>
 
-<h4>Epson</h4>
+<!-- ========================= -->
+
+<h3>Epson</h3>
 <p>Epson advertise us in the 
 <a href="http://www.epsondevelopers.com/ldcam.jsp">Linux developer solution section</a>,
 but never, as far as we remember,
@@ -71,7 +96,9 @@ and scanners, with a <a href="http://www.epsondevelopers.com/noGuide.jsp">long l
 
 <p>They haven't produced anything recently, and the Epson RD-1 does not feature connectivity.</p>
 
-<h4>Olympus</h4>
+<!-- ========================= -->
+
+<h3>Olympus</h3>
 <p>When requesting the documentation because their SDK only support Windows, 
 here is the answer I got on June 22, 2005:</p>
 <pre class="programlisting">
@@ -94,11 +121,16 @@ But for remote control,
 <a href="http://developer.olympus.com/cameras_25.html">supported cameras</a> needs documentation
 and that is what they are currently declining.</p>
 
-<h4>Nikon</h4>
+<!-- ========================= -->
+
+<h3>Nikon</h3>
 
 <p>Documentation has been requested, but require signing a NDA. Nikon has been requested clarifications
 about the NDA (sent June 17 2005) to know if Open Source software can be written from it, no answer so 
 far (June 22 2005)...</p>
+
+
+<!-- ============================================================ --> 
 
 <?
 	printFooter ();
