8757c6562439da54ca460b9073247f384bf60521
Hans Ulrich Niedermann <hun@n-dimensional.de>
2005-06-23 13:31:47 +0000

    just say 'hundreds of cameras'
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/website@8133 67ed7778-7388-44ab-90cf-0a291f65f57c

--- proj/index.php
+++ proj/index.php
@@ -9,24 +9,25 @@
 <h2>libgphoto2</h2>
 <p class="text">
 libgphoto2 is the core of gphoto2 software. It is a portable library
-to gives access to more than <a href="libgphoto2/support.php">400 digital cameras.</a></p>
-<p class="more"><a href="libgphoto2/">(more...)</a></p>
+which gives access to literally
+<a href="libgphoto2/support.php">hundreds of digital cameras</a>.</p>
+<p class="more"><a href="libgphoto2/" title="more about libgphoto2">(more...)</a></p>
 
 <h2>gphoto2</h2>
 <p class="text">
 gphoto2 is the command line interface to libgphoto2. It allows almost 
 everything that libgphoto2 can do.</p>
-<p class="more"><a href="gphoto2/">(more...)</a></p>
+<p class="more"><a href="gphoto2/" title="more about gphoto2">(more...)</a></p>
 <h2>gtkam</h2>
 <p class="text">
 gtkam is the GTK2 graphical front-end to libgphoto2.</p>
-<p class="more"><a href="gtkam/">(more...)</a></p>
+<p class="more"><a href="gtkam/" title="more about gtkam">(more...)</a></p>
 
 <h2>gphoto 0.4.x</h2>
 <p class="text">
 gphoto 0.4.x is the old fashionned version of gphoto software. It is
 not longer being maintained.</p>
-<p class="more"><a href="gphoto/">(more...)</a></p>
+<p class="more"><a href="gphoto/" title="more about gphoto">(more...)</a></p>
 <?
 	printFooter ();
 ?>
