197b60bbc2e07610780ed73bb5ad2f660a35dcd7
Hubert Figuière <hub@figuiere.net>
2005-12-29 22:12:43 +0000

    more info
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/website@8362 67ed7778-7388-44ab-90cf-0a291f65f57c

--- proj/gphotofs/index.php
+++ proj/gphotofs/index.php
@@ -12,6 +12,9 @@ filesystem module to mount your camera as a filesystem on Linux. This
 allow using your camera with any tool able to read from a mounted
 filesystem.</p>
 
+<p>gphotofs is written by Philip Langdale. See the <a
+href="http://intr.overt.org/blog/?p=25">original announcement</a>.</p>
+
 <?
 	printFooter ();
 ?>
