4f73207926ad6bb67e68d9d1d3f25b4e8751b6f6
Marcus Meissner <marcus@jet.franken.de>
2006-06-18 21:03:29 +0000

    700 cameras. changed SuSE to SUSE.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/website@8893 67ed7778-7388-44ab-90cf-0a291f65f57c

--- index.php
+++ index.php
@@ -75,10 +75,10 @@
     ready to use set of digital camera software
     applications for Unix-like systems, written by a whole team of dedicated
     volunteers around the world. It supports more than 
-    <a href="/proj/libgphoto2/support.php">600 cameras</a></p>
+    <a href="/proj/libgphoto2/support.php">700 cameras</a></p>
     <p>gPhoto2 runs on a large range of UNIX-like operating system, including
     Linux, FreeBSD, NetBSD, etc. gPhoto is provided by major Linux distributions
-    like Debian GNU/Linux, Ubuntu, Gentoo, Fedora Core, SuSE Linux, Mandriva, etc.</p>
+    like Debian GNU/Linux, Ubuntu, Gentoo, Fedora Core, SUSE Linux, Mandriva, etc.</p>
     <p>libgphoto2 is freely available and distributed
     under the terms of the 
     <a href="http://www.gnu.org/copyleft/lesser.html">GNU LGPL.</a>; the other gphoto
