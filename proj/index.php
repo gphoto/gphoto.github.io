c748f0d246973b73c26d9870e2073a7bf53da839
Marcus Meissner <marcus@jet.franken.de>
2011-09-27 11:56:47 +0000

    added external projects using libgphoto2
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/website@13724 67ed7778-7388-44ab-90cf-0a291f65f57c

--- proj/index.php
+++ proj/index.php
@@ -15,23 +15,123 @@ which gives access to literally
 
 <h2>gphoto2</h2>
 <p class="text">
-gphoto2 is the command line interface to libgphoto2. It allows almost 
+gphoto2 is the command line (CLI) interface to libgphoto2. It allows almost 
 everything that libgphoto2 can do.</p>
 <p class="more"><a href="gphoto2/" title="more about gphoto2">(more...)</a></p>
 
+
+<h2>DigiKam</h2>
+<p class="text">
+digiKam is an advanced digital photo management application for Linux,
+Windows, and Mac-OSX.
+
+The people who inspired digiKam's design are the photographers like
+you who want to view, manage, edit, enhance, organize, tag, and share
+photographs under Linux systems.</p>
+<p class="text">
+It uses libgphoto2 for importing photos and for capture.
+</p>
+<p class="more"><a href="http://www.digikam.org">DigiKam</a></p>
+
+<h2>Shotwell</h2>
+<p class="text">
+Shotwell is a digital photo organizer that runs on Linux.
+</p>
+<p class="more">
+<a href="http://yorba.org/shotwell/">Shotwell</a>
+</p>
+
+<h2>F-Spot</h2>
+<p class="text">
+F-Spot is a full-featured personal photo management application for the GNOME desktop.
+F-Spot simplifies digital photography by providing intuitive tools to
+help you share, touch-up, find and organize your images.
+</p>
+<p class="more">
+<a href="http://f-spot.org/">F-Spot</a>
+</p>
+
+<h2>Google Picasa</h2>
+<p class="text">
+Google Picasa has been ported to Linux using Wine which uses libgphoto2
+for importing images.
+</p>
+<p class="more">
+<a href="http://picasa.google.com/linux/">Picasa for Linux</a>
+</p>
+
+
+<h2>Darktable</h2>
+<p class="text">
+darktable is a photography workflow application: a virtual lighttable
+and darkroom for photographers: it manages your digital negatives in a
+database and lets you view them through a zoomable lighttable. it also
+enables you to develop raw images and enhance them.</p>
+<p class="text">
+It uses the capture abilities of libgphoto2.</p>
+<p class="more"><a href="http://darktable.sourceforge.net">Darktable</a></p>
+
+<h2>Entangle</h2>
+<p class="text">
+Entangle provides a graphical interface for tethered shooting,
+aka taking photographs with a digital camera completely controlled from
+the computer.</p>
+<p class="more"><a href="http://entangle-photo.org">Entangle</a></p>
+
+<h2>GIMP</h2>
+<p class="text">
+GIMP is the GNU Image Manipulation Program. It is a freely distributed
+piece of software for such tasks as photo retouching, image composition
+and image authoring. It works on many operating systems, in many
+languages.
+</p>
+<p class="more">
+<a href="http://www.gimp.org/=">Gimp</a>
+</p>
+
+
+<h2>EOS Movie Record</h2>
+<p class="text">
+This software writes short movies with your digital DSLR camera Canon
+(R) directly to computer. The camera must have LiveView feature to work,
+like in Canon EOS 450D for example. The Program has preview, Av, Tv and
+WB control.
+</p>
+<p class="more"><a href="http://eos-movrec.sourceforge.net">EOS Camera Movie Record</a></p>
+
+
+<h2>Stop-Motion Capture</h2>
+<p class="text">
+A program to create stop motion animations under Linux. (website is in german currently).
+</p>
+<p class="more">
+<a href="http://mafrasi.sistemich.de/stopmotion.php">Stop-Motion Capture</a>
+</p>
+
+
+<h2>Python bindings for libgphoto2</h2>
+<p class="text">
+The site provides python bindings and some sample for libgphoto2.
+</p>
+<p class="more">
+<a href="http://magiclantern.wikia.com/wiki/Remote_control_with_PTP_and_Python">Magic Lantern</a>
+</p>
+
+
 <h2>gtkam</h2>
 <p class="text">
-gtkam is the GTK2 graphical front-end to libgphoto2.</p>
+gtkam is the old GTK2 reference graphical front-end to libgphoto2.
+It is not maintained currently (volunteers welcome).</p>
 <p class="more"><a href="gtkam/" title="more about gtkam">(more...)</a></p>
 
 <h2>gphotofs</h2>
 <p class="text">
-gphotofs allow you no mount your camera on Linux using FUSE.</p>
+gphotofs allow you no mount your camera on Linux based on the FUSE technology.</p>
 <p class="more"><a href="gphotofs/" title="more about gphotofs">(more...)</a></p>
 
 <h2>gphoto 0.4.x</h2>
 <p class="text">
-gphoto 0.4.x is the old fashionned version of gphoto software. It is
+gphoto 0.4.x is the old fashioned version of gphoto software. It is
 not longer being maintained.</p>
 <p class="more"><a href="gphoto/" title="more about gphoto">(more...)</a></p>
 <?
