e0d329ee5163d9f4c93e8d286a0900242c256dfe
Marcus Meissner <marcus@jet.franken.de>
2013-04-12 07:01:03 +0000

    updated the links page, removed outdated links and refreshed the usb sniffer list
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/website@14335 67ed7778-7388-44ab-90cf-0a291f65f57c

--- links/index.php
+++ links/index.php
@@ -19,75 +19,43 @@
    <p>For Linux USB setup information, check the 
    <a href="http://www.linux-usb.org/">Linux USB</a> website and the 
    <a href="http://www.linux-usb.org/USB-guide/book1.html">Linux USB Guide</a>.
-   Also <a href="http://www.harald-schreiber.de/">Using a USB Mass Storage Camera</a>.
    </p>
    
-<?
-/*
-   <p>If you have a Connectix QuickCam, then
-   check out <a href= 
-   "http://www.dkfz-heidelberg.de/Macromol/wedemann/mini-HOWTO-cqcam.html">
-    http://www.dkfz-heidelberg.de/Macromol/wedemann/mini-HOWTO-cqcam.html</a></p>
-    */
-?>
-   <p>OpenDiS, the
-   <a href="http://digitalux.netpedia.net/">Open Digita Services</a>
-   project, is an effort to write
-   free tools for accessing Digita cameras from
-   Linux and other unix operating systems.
-   Initial targets for support are the Kodak
-   DC-220 and DC-260 digital cameras.</p>
-		      
    <p>Also checkout the <a href="http://www.tldp.org/HOWTO/Kodak-Digitalcam-HOWTO/index.html">Kodak Digital Camera HOWTO</a>.</p>
 
    <p><a href="http://www.sane-project.org/">SANE</a>
-   stands for 'Scanner Access Now Easy'
-   and is an application programming interface
-   (API) that provides standardized access to
-   raster image scanner hardware.</p>
+   stands for 'Scanner Access Now Easy' and is an application programming interface
+   (API) that provides standardized access to raster image scanner hardware.
+   </p>
 
-   <p><a href="http://www.wingmanteam.com/usbsnoopy/">Snoopy</a> an USB snooper
-   software for Microsoft Windows. This tool is used to help reverse
-   engineering protocols of cameras runing over USB.</p>
+
+   <h3>USB Sniffers for Windows</h3>
+
+   <ul>
+   <li><a href="http://www.usblyzer.com/">USBLyzer</a> - best current product. Commercial, with a 30 day free trial.
+   <li><a href="http://sourceforge.net/projects/usbsnoop/">SnoopyPro</a> - another Windows USB snoop tool.
+   <li><a href="https://www.wireshark.org/">Wireshark</a> - the opensource network and USB sniffer. (Linux users: <code>modprobe usbmon</code> to enable USB sniffing)
+   <li><a href="http://desowin.org/usbpcap">USBPcap</a> - opensource pcap port for Windows
+   </ul>
 
    <h3>Digital camera review/resources</h3>
 
    <ul>
-      <li><a href="http://www.dcresource.com/">DCResource</a></li>
+      <li><a href="http://www.dpreview.com/">DPreview</a></li>
+      <li><a href="http://www.kenrockwell.com/">Ken Rockwell</a></li>
    </ul>
 
-   <h3>Free software</h3>
+   <h3>Free software related to digital images</h3>
 
    <ul>
-      <li><a href="http://www.gimp.org/">
-      www.gimp.org</a> - The GNU Image Manipulation
-      Program</li>
-
-      <li><a href="http://www.gnu.org/">
-      www.gnu.org</a> - GNU's Not Unix!</li>
-
-      <li><a href="http://www.gnome.org/">
-      www.gnome.org</a> - GNU Network Object Model
-      Environment</li>
-
-      <li><a href="http://www.gtk.org/">
-      www.gtk.org</a> - The Gimp Toolkit</li>
-
-      <li><a href="http://www.kernel.org/">
-      www.kernel.org</a> - Latest Linux kernel</li>
-   </ul>
-
-   <h3>News</h3>
-
-   <ul>
-      <li><a href="http://www.uk.linux.org/">uk.linux.org</a></li>
-
-      <li><a href="http://www.freshmeat.net/">Freshmeat</a></li>
+      <li><a href="http://www.gimp.org/"> www.gimp.org</a> - The GNU Image Manipulation Program</li>
+      <li><a href="http://www.digikam.org/"> www.digikam.org</a> - KDE Digikam</li>
+      <li><a href="http://www.darktable.org/"> www.darktable.org</a> - OpenSource Darktable (inspired by Adobe Lightroom)</li>
    </ul>
 
    <h3>Vendor packages</h3>
    <ul>
-     <li>SUSE, Fedora and Mandriva <a href="http://software.opensuse.org/download/GPhoto">gphoto RPM packages courtesy of the opensuse build service</a> (<a href="http://build.opensuse.org/project/show?project=GPhoto">internal opensuse build service page for gphoto</a>)</li>
+     <li>SUSE, Fedora and Mandriva <a href="http://software.opensuse.org/package/libgphoto2">gphoto RPM packages courtesy of the opensuse build service</a> (<a href="http://build.opensuse.org/project/show?project=GPhoto">internal opensuse build service page for gphoto</a>)</li>
      <li><a href="http://packages.debian.org/gphoto2">Debian gphoto2 package page</a></li>
      <li><a href="http://packages.ubuntulinux.org/gphoto2">Ubuntu gphoto2 package page</a></li>
    </ul>
